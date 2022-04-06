701767e122390442df2e51ad7d4d78198cc8f27c
MuhammadFadll <fadeljourney@gmail.com>
2022-04-06 10:29:40 +0700

    useReducer step one

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-// import HooksState from "./components/HooksState";
-// import HookCounterThree from "./components/HookCounterThree";
-// import HookCounterFour from "./components/HookCounterFour";
-// import HookEffect from "./components/HookEffect";
-// import HookMouse from "./components/HookMouse";
-// import HookContainer from "./components/HookContainer";
-// import DataFetching from "./components/DataFetching";
 import React from "react";
-import ComponentC from "./components/ComponentC";
-
-export const UserContext = React.createContext();
+import "./App.css";
+import CounterOne from "./components/CounterOne";
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={"fadel"}>
-        <ComponentC />
-      </UserContext.Provider>
+      <CounterOne />
     </div>
   );
 }
